873e1fa6a63555fc0623719ae918b88a7a10a350
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-17 04:19:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-//your JS code here. If required.
+const output = document.querySelector("#output");
+const btn = document.querySelector("#btn");
+const inputNumber = document.querySelector('input[type="number"]');
+
+const wait = function (time, result, type = "", value = "") {
+  return new Promise(function (resolve, _) {
+    setTimeout(function () {
+      if (type == "+") resolve(result + value);
+      else if (type == "-") resolve(result - value);
+      else if (type == "*") resolve(result * value);
+      else if (type == "/") {
+        if (value == 0) throw new Error("cannot divide by zero");
+        resolve(result / value);
+      }
+      resolve(result);
+    }, time * 1000);
+  });
+};
+
+btn.addEventListener("click", function () {
+  try {
+    wait(2, parseInt(inputNumber.value))
+      .then((result) => {
+        const html = `Result: ${result}`;
+        output.insertAdjacentHTML("afterbegin", html);
+        return wait(1, result, "*", 2);
+      })
+      .then((result) => {
+        output.removeChild(output.firstChild);
+        const html = `Result: ${result}`;
+        output.insertAdjacentHTML("afterbegin", html);
+        return wait(1, result, "-", 3);
+      })
+      .then((result) => {
+        output.removeChild(output.firstChild);
+        const html = `Result: ${result}`;
+        output.insertAdjacentHTML("afterbegin", html);
+        return wait(1, result, "/", 2);
+      })
+      .then((result) => {
+        output.removeChild(output.firstChild);
+        const html = `Result: ${result}`;
+        output.insertAdjacentHTML("afterbegin", html);
+        return wait(1, result, "+", 10);
+      })
+      .then((result) => {
+        output.removeChild(output.firstChild);
+        const html = `Final Result: ${result}`;
+        output.insertAdjacentHTML("afterbegin", html);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+});
